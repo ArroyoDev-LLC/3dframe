e7ea546922fd38c6cb1b2706d2dd33be29a835fe
Braden Mars <bradenmars@bradenmars.me>
2021-02-06 03:12:15 -0600

    feat(utils): add MeshPoint, MeshFace, MeshData dataclass utils

--- threedframe/utils.py
+++ threedframe/utils.py
@@ -2,18 +2,21 @@
 
 """3DFrame Model Generator Utils."""
 import sys
+import json
 import time
 import string
+import itertools
 import subprocess as sp
-from typing import Dict, List, Tuple, Union, Callable, Iterator, Sequence
+from typing import Dict, List, Tuple, Union, Callable, Iterator, Optional, Sequence
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from dataclasses import field, dataclass
 
+import numpy as np
 import sympy as S
 from rich import print
 from solid import OpenSCADObject, text, union, resize, translate, scad_render, linear_extrude
-from euclid3 import Point3
+from euclid3 import Point3, Vector3
 from rich.text import Text
 from rich.console import RenderableType
 from rich.progress import Task, TextColumn, SpinnerColumn, ProgressColumn
@@ -136,6 +139,134 @@ class ModelVertex:
     edges: List[ModelEdge]
     # Generated label for vertex.
     label: str = field(default_factory=lambda: next(MODEL_LABELS))
+
+
+@dataclass
+class MeshPoint:
+    """Mesh Point."""
+
+    # Vertex Index.
+    vidx: int
+    # 3D Coordinate of point.
+    point: Tuple[float, float, float]
+    # Point normal.
+    normal: Optional[Tuple[float, float, float]]
+    # Parent Mesh Data.
+    _parent: Optional["MeshData"] = field(repr=False, hash=False, default=None)
+
+    @property
+    def faces(self) -> List["MeshFace"]:
+        return [f for f in self._parent.faces if self.vidx in f.vertex_indices]
+
+    @property
+    def as_euclid(self) -> Point3:
+        return Point3(*self.point)
+
+    @property
+    def as_sympy(self) -> S.Point3D:
+        return S.Point3D(*self.point)
+
+    @property
+    def as_vector(self) -> Vector3:
+        return Vector3(*self.point)
+
+    @property
+    def normal_vector(self) -> Vector3:
+        return Vector3(*self.normal)
+
+
+@dataclass
+class MeshFace:
+    """Mesh Face."""
+
+    # Face Index.
+    fidx: int
+    # Vertices that makeup face.
+    vertex_indices: List[int]
+    # Face normal.
+    normal: Optional[Tuple[float, float, float]]
+    # Face area.
+    area: float
+    # Vector representing face centroids.
+    centroid: Optional[Tuple[float, float, float]]
+    # Parent Mesh Data.
+    _parent: Optional["MeshData"] = field(repr=False, hash=False, default=None)
+
+    @property
+    def vertices(self) -> List[MeshPoint]:
+        return [v for v in self._parent.vertices if v.vidx in self.vertex_indices]
+
+    @property
+    def sympy_vertices(self) -> List[S.Point3D]:
+        return [p.as_sympy for p in self.vertices]
+
+    @property
+    def euclid_vertices(self) -> List[Point3]:
+        return [p.as_euclid for p in self.vertices]
+
+    @property
+    def as_sympy_plane(self) -> S.Plane:
+        return S.Plane(*self.sympy_vertices, normal_vector=self.normal)
+
+    @property
+    def missing_rect_vertex(self) -> S.Point3D:
+        points_perms = itertools.permutations(self.sympy_vertices, 3)
+        least_dist = None
+        least_dist_point = None
+        for perm in points_perms:
+            midp = find_missing_rect_vertex(*perm)
+            dist = self.centroid_point.distance(midp)
+            if least_dist is None or least_dist > dist:
+                least_dist = dist
+                least_dist_point = midp
+        return least_dist_point
+
+    @property
+    def midpoint_by_canberra(self) -> S.Point3D:
+        fp_1 = self.vertices[0].as_sympy
+        fp_2 = max(self.sympy_vertices, key=lambda p: fp_1.canberra_distance(p))
+        return fp_1.midpoint(fp_2)
+
+    @property
+    def normal_vector(self) -> Vector3:
+        return Vector3(*self.normal)
+
+    @property
+    def centroid_point(self) -> S.Point3D:
+        return S.Point3D(*self.centroid)
+
+    # def calc_absolute_midpoint(self):
+    #     return find_center_of_gravity(*self.sympy_vertices)
+
+
+@dataclass
+class MeshData:
+    """Mesh Analysis Data."""
+
+    # Vertices of mesh.
+    vertices: List[MeshPoint]
+    # Faces of mesh.
+    faces: List[MeshFace]
+
+    def __post_init__(self):
+        for vert in self.vertices:
+            vert._parent = self
+        for face in self.faces:
+            face._parent = self
+
+    @classmethod
+    def from_dict(cls, data):
+        vertices = data.get("vertices", [])
+        faces = data.get("faces", [])
+        verts = [MeshPoint(**v) for v in vertices]
+        fces = [MeshFace(**f) for f in faces]
+        return cls(vertices=verts, faces=fces)
+
+    def calc_absolute_midpoint(self):
+        face_mps = []
+        for face in self.faces:
+            face_mps.append(find_center_of_gravity(*face.sympy_vertices, face.missing_rect_vertex))
+        return find_center_of_gravity(*face_mps)
 
 
 def round_point(point: Point3, n_digits=2):
