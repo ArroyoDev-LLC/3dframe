2372cf124ed02f6d030b0bf568d00a15cb390df8
Braden Mars <bradenmars@bradenmars.me>
2021-05-15 05:38:21 -0500

    fix(constants): make Constants a float-type enum

--- threedframe/constant.py
+++ threedframe/constant.py
@@ -1,9 +1,9 @@
 """3DFrame Constants."""
 
-from enum import IntEnum
+from enum import Enum
 
 
-class Constants(IntEnum):
+class Constants(float, Enum):
     TAU = 6.2831853071  # 2*PI
     MM = 1
     INCH = 25.4 * MM
