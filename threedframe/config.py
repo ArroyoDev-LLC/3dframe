8653d7d3a498c2e931a052db76b027ba0e4ed6d0
Braden Mars <bradenmars@bradenmars.me>
2021-05-15 06:01:28 -0500

    feat(config): add quiet_solid contextmanager, init mcad/dotSCAD during config init

--- threedframe/config.py
+++ threedframe/config.py
@@ -1,15 +1,34 @@
 """3DFrame configuration."""
 
+import contextlib
+from typing import Any, Optional
 from pathlib import Path
 
 import solid
 from pydantic import BaseSettings, validator
+from pydantic.fields import PrivateAttr
 
 from threedframe.unit import Unit, UnitMM
 
 __all__ = ["config"]
 
 ROOT = Path(__file__).parent
+
+
+@contextlib.contextmanager
+def quiet_solid():
+    """Monkey patch solidpython print statements.
+
+    The errors are known and accommodated for,
+    so just ignore them.
+
+    """
+    solid.objects.print = lambda *args: None
+    solid.solidpython.print = lambda *args: None
+    yield
+    # undo monkey patch.
+    solid.objects.print = print
+    solid.solidpython.print = print
 
 
 class _Config(BaseSettings):
@@ -20,19 +39,32 @@ class _Config(BaseSettings):
     MCAD_DIR: Path = LIB_DIR / "MCAD"
     DOTSCAD_DIR: Path = LIB_DIR / "dotSCAD"
 
+    _mcad: Optional[Any] = PrivateAttr(None)
+    _dotSCAD: Optional[Any] = PrivateAttr(None)
+
     @validator("LIB_DIR", "MCAD_DIR", "DOTSCAD_DIR")
     def validate_libs(cls, v: Path) -> Path:
         if not v.exists():
             raise ValueError(f"Missing library: {v}")
-        # Monkey patch solidpython print statements.
-        # (the errors are known and accommodated for)
-        solid.objects.print = lambda *args: None
-        solid.solidpython.print = lambda *args: None
-        solid.import_scad(str(v))
-        # undo monkey patch.
-        solid.objects.print = print
-        solid.solidpython.print = print
         return v
+
+    @validator("MCAD_DIR")
+    def validate_mcad(self):
+        with quiet_solid():
+            self._mcad = solid.import_scad(str(self.MCAD_DIR))
+
+    @validator("DOTSCAD_DIR")
+    def validate_mcad(self):
+        with quiet_solid():
+            self._dotSCAD = solid.import_scad(str(self.DOTSCAD_DIR))
+
+    @property
+    def mcad(self):
+        return self._mcad
+
+    @property
+    def dotSCAD(self):
+        return self._dotSCAD
 
     # Model Params
     GAP: UnitMM = UnitMM(0.02)  # 3dPrinting fudge factor.
