cbbb9db0859766c1094415404973c01b2ab9d99d
Braden Mars <bradenmars@bradenmars.me>
2021-05-16 06:14:19 -0500

    test: update path to json

--- tests/test_scad.py
+++ tests/test_scad.py
@@ -7,7 +7,7 @@ from threedframe import joint
 
 @pytest.mark.parametrize("vertices", [(1,), (2,), (3,)])
 def test_scad_render(test_data: Path, snapshot, vertices):
-    in_data = test_data / "computed.pkl"
+    in_data = test_data / "computed.json"
     assert in_data.exists()
     joint.generate(in_data, vertices=vertices, render=False, keep=True)
     render_dir = joint.ROOT.parent / "renders"
