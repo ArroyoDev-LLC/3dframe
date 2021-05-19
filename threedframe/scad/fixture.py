fa9b1279963db418933b3e443327b7a9aa0195e7
Braden Mars <bradenmars@bradenmars.me>
2021-05-19 03:37:00 -0500

    feat(scad): add fixture module with concrete Fixture class

---
+++ threedframe/scad/fixture.py
@@ -0,0 +1,24 @@
+import attr
+import solid as sp
+from solid import utils as sputils
+
+from threedframe.config import config
+from threedframe.scad.interfaces import FixtureMeta
+
+
+@attr.s(auto_attribs=True)
+class Fixture(FixtureMeta):
+    def create_base(self) -> sp.OpenSCADObject:
+        return sp.square(config.FIXTURE_SIZE, center=True)
+
+    def do_extrude(self, obj: sp.OpenSCADObject):
+        obj = config.dotSCAD.hollow_out.hollow_out(shell_thickness=3)(obj)
+        obj = sp.linear_extrude(self.params.extrusion_height)(obj)
+        return obj
+
+    def do_transform(self, obj: sp.OpenSCADObject):
+        obj = sputils.transform_to_point(
+            obj, dest_point=self.params.midpoint, dest_normal=self.params.vector_from_origin
+        )
+        return obj
+
