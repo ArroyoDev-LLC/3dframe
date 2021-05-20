79d2894875c8eb6d45442f0edc5ec712af47366e
Braden Mars <bradenmars@bradenmars.me>
2021-05-20 04:00:40 -0500

    feat(scad): add JointMeta abstract class

--- threedframe/scad/interfaces.py
+++ threedframe/scad/interfaces.py
@@ -77,3 +77,32 @@ class LabelMeta(ScadMeta, abc.ABC):
         obj = self.do_transform(obj)
         self.scad_object = obj
 
+
+@attr.s(auto_attribs=True)
+class JointMeta(ScadMeta, abc.ABC):
+    meshes: Dict[str, "MeshData"] = attr.ib(init=False)
+    core: "CoreMeta" = attr.ib(init=False)
+    fixtures: List["FixtureMeta"] = attr.ib(init=False)
+
+    fixture_builder: Type["FixtureMeta"] = ...
+    fixture_label_builder: Type["LabelMeta"] = ...
+    core_builder: Type["CoreMeta"] = ...
+    # core_label_builder: Type['LabelMeta'] = ...
+
+    vertex: "ModelVertex" = ...
+
+    @abc.abstractmethod
+    def build_fixture_params(self) -> Iterator["FixtureParams"]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def build_fixtures(self) -> Iterator[FixtureMeta]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def build_fixture_meshes(self) -> Iterator[Tuple[str, "MeshData"]]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def build_core(self) -> "CoreMeta":
+        raise NotImplementedError
