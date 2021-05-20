381f3e1822879dfb660707b4623ffc65b90a9335
Braden Mars <bradenmars@bradenmars.me>
2021-05-20 04:29:30 -0500

    feat(scad): update core concrete to not create meshes, add DebugCoreMesh concrete

--- threedframe/scad/core.py
+++ threedframe/scad/core.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, List, Tuple, Iterator
+from typing import TYPE_CHECKING, List, Tuple, Union, Iterator
 
 import attr
 import solid as sp
@@ -6,23 +6,15 @@ import sympy as S
 from solid import utils as sputils
 
 from threedframe import utils
-from threedframe.models.mesh import analyze_scad
-from threedframe.scad.fixture import Fixture, SolidFixture
 from threedframe.scad.interfaces import CoreMeta
 
 if TYPE_CHECKING:
     from threedframe.models import MeshFace
+    from threedframe.scad.interfaces import FixtureMeta
 
 
 @attr.s(auto_attribs=True)
 class Core(CoreMeta):
-    @staticmethod
-    def create_solid_fixture(fixture: Fixture):
-        params = fixture.params
-        fix = SolidFixture(params=params)
-        fix.assemble()
-        return fix
-
     @staticmethod
     def find_inner_face_midpoint(points: List[S.Point]) -> Tuple[S.Point, List[S.Point]]:
         """Find midpoint of inner (closest to core) face."""
@@ -36,27 +28,44 @@ class Core(CoreMeta):
         return face_midpoint, points
 
     def create_vertex_cube(
-        self, face: "MeshFace", face_midpoint: S.Point, vertex: S.Point
+        self,
+        face: "MeshFace",
+        face_midpoint: S.Point,
+        vertex: S.Point,
+        *,
+        color: Union[str, Tuple[int, ...]] = "red",
     ) -> sp.OpenSCADObject:
         # First face is nearest to origin.
         face_norm = face.normal
-        cube_obj = sp.color("red")(sp.cube(1, center=True))
+        cube_color = sp.color(color) if isinstance(color, str) else sp.color(c=color)
+        cube_obj = cube_color(sp.cube(1, center=True))
         # Scale corner point with reference to midpoint to 'inset' cubes into corners
         scaled_point = vertex.scale(0.9575, 0.9575, 0.9575, pt=face_midpoint)
         cube_obj = sputils.transform_to_point(
-            cube_obj, dest_point=tuple(scaled_point), dest_normal=tuple(face_norm)
+            cube_obj, dest_point=tuple(scaled_point), dest_normal=face_norm
         )
         return cube_obj
 
-    def create_fixture_inner_vertex_cubes(self, fixture: Fixture) -> Iterator[sp.OpenSCADObject]:
-        solid_fix = self.create_solid_fixture(fixture)
-        solid_mesh = analyze_scad(solid_fix.scad_object)
+    def create_fixture_inner_vertex_cubes(
+        self,
+        fixture: "FixtureMeta",
+        **kwargs,
+    ) -> Iterator[sp.OpenSCADObject]:
+        solid_mesh = self.meshes[fixture.params.label]
         face = solid_mesh.faces[0]
         vertices = [v.as_sympy for v in face.vertices]
         face_midpoint, all_vertices = self.find_inner_face_midpoint(vertices)
         for vertex in all_vertices:
-            yield self.create_vertex_cube(face, face_midpoint, vertex)
+            yield self.create_vertex_cube(face, face_midpoint, vertex, **kwargs)
 
     def create_hull_cubes(self) -> Iterator[sp.OpenSCADObject]:
+        color_gen = utils.rand_color_generator()
         for fix in self.fixtures:
-            yield from self.create_fixture_inner_vertex_cubes(fix)
+            yield from self.create_fixture_inner_vertex_cubes(fix, color=next(color_gen))
+
+
+class CoreDebugCubes(Core):
+    def assemble(self):
+        fixture_vertex_cubes = list(self.create_hull_cubes())
+        obj = sp.union()(*fixture_vertex_cubes)
+        self.scad_object = obj
