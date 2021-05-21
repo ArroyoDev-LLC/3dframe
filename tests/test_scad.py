1839f647a53bc98f4c87e5b62f052f5b5070ae6e
Braden Mars <bradenmars@bradenmars.me>
2021-05-20 23:09:28 -0500

    test(scad): update snapshots tests

--- tests/test_scad.py
+++ tests/test_scad.py
@@ -3,13 +3,16 @@ from pathlib import Path
 import pytest
 
 from threedframe import joint
+from threedframe.scad import JointDirector, JointDirectorParams
 
 
 @pytest.mark.parametrize("vertices", [(1,), (2,), (3,)])
 def test_scad_render(test_data: Path, snapshot, vertices):
     in_data = test_data / "computed.json"
     assert in_data.exists()
-    joint.generate(in_data, vertices=vertices, render=False, keep=True)
+    params = JointDirectorParams(model=in_data, vertices=[1])
+    director = JointDirector(params=params)
+    director.assemble()
     render_dir = joint.ROOT.parent / "renders"
     out_path = render_dir / "joint-v1.scad"
     assert out_path.exists()
