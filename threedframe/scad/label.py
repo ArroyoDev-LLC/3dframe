c2e262163059d7ab746a1ce0b0f5bcc327a1a809
Braden Mars <bradenmars@bradenmars.me>
2021-07-05 13:27:18 -0500

    refactor(scad/label): migrate fixture-label code to FixtureLabelParams class

--- threedframe/scad/label.py
+++ threedframe/scad/label.py
@@ -1,5 +1,6 @@
 import math
-from typing import TYPE_CHECKING, Any, Dict, List, Union, Optional
+from typing import TYPE_CHECKING, Any, Dict, List, Tuple, Union, Optional
+from functools import cached_property
 
 import attr
 import solid as sp
@@ -9,19 +10,21 @@ from loguru import logger
 from euclid3 import Line3 as EucLine3
 from euclid3 import Point3 as EucPoint3
 from euclid3 import Vector3 as EucVector3
-from pydantic import Field, BaseModel
+from pydantic import Field, BaseModel, PrivateAttr
 
 from threedframe import utils
 from threedframe.config import config
 from threedframe.scad.interfaces import LabelMeta
 
+from ..models import MeshData, MeshFace
+from .interfaces import FixtureMeta
+
 if TYPE_CHECKING:
-    from ..models import MeshData, MeshFace
-    from .interfaces import FixtureMeta
+    from pydantic.typing import DictStrAny
 
 
 class LabelParams(BaseModel):
-    content: str
+    content: Union[str, Tuple[str, str, str]]
     halign: str = "center"
     valign: str = "center"
     depth: float = 1.5
@@ -30,14 +33,22 @@ class LabelParams(BaseModel):
     center: bool = True
 
 
-@attr.s(auto_attribs=True, kw_only=True)
-class FixtureLabel(LabelMeta):
-    fixtures: List["FixtureMeta"] = ...
-    target: "FixtureMeta" = ...
-    meshes: Dict[str, "MeshData"] = ...
+class FixtureLabelParams(LabelParams):
+    fixtures: List[FixtureMeta] = ...
+    target: FixtureMeta = ...
+    meshes: Dict[str, MeshData] = ...
 
-    midpoint: Optional[S.Point] = None
-    target_face: Optional["MeshFace"] = None
+    _midpoint: Optional[S.Point] = PrivateAttr(default=None)
+    _target_face: Optional[MeshFace] = PrivateAttr(default=None)
+
+    class Config:
+        keep_untouched = (cached_property,)
+        arbitrary_types_allowed = True
+
+    def dict(self, **kws) -> "DictStrAny":
+        base_kws = dict(exclude={"fixtures", "target", "meshes"})
+        base_kws.update(kws)
+        return super().dict(**base_kws)
 
     @property
     def target_label(self) -> str:
@@ -78,6 +89,21 @@ class FixtureLabel(LabelMeta):
         if self.target_face and self.destination_offset:
             return self.midpoint_euc.copy() + self.destination_offset.copy()
 
+    @property
+    def midpoint(self) -> Optional[S.Point3D]:
+        if not self._midpoint and self.target_face is not None:
+            # now that we have found an appropriate face, find the center of it to place the label.
+            self._midpoint = utils.find_center_of_gravity(
+                *self.target_face.sympy_vertices, self.target_face.missing_rect_vertex
+            )
+        return self._midpoint
+
+    @property
+    def target_face(self) -> Optional["MeshFace"]:
+        if not self._target_face:
+            self._target_face = self.find_clear_face()
+        return self._target_face
+
     def find_clear_face(self) -> "MeshFace":
         nearest_origin_face = self.target_mesh.faces[0]
         other_centers = [m.absolute_midpoint for m in self.other_meshes.values()]
@@ -98,43 +124,41 @@ class FixtureLabel(LabelMeta):
                     break
         return current_optimal_face
 
+
+@attr.s(auto_attribs=True, kw_only=True)
+class FixtureLabel(LabelMeta):
+    params: FixtureLabelParams = ...
+
     def do_transform(self, obj: sp.OpenSCADObject) -> sp.OpenSCADObject:
-        self.target_face = self.find_clear_face()
-
-        # now that we have found an appropriate face, find the center of it to place the label.
-        self.midpoint = utils.find_center_of_gravity(
-            *self.target_face.sympy_vertices, self.target_face.missing_rect_vertex
-        )
-
         logger.debug(
             "Translating label from face midpoint {} to destination {}.",
-            self.midpoint_euc,
-            self.destination_point,
+            self.params.midpoint_euc,
+            self.params.destination_point,
         )
 
         trans_params = dict(
-            dest_point=self.destination_point,
-            dest_normal=-self.target_face.normal_vector,
-            src_up=self.target_face.normal_vector,
-            src_point=self.label_init_point,
+            dest_point=self.params.destination_point,
+            dest_normal=-self.params.target_face.normal_vector,
+            src_up=self.params.target_face.normal_vector,
+            src_point=self.params.label_init_point,
         )
         logger.info(
             "[{sl}@{tl}] Transforming to ({dest_point}) w/ normal @ ({dest_normal} | rnd: {dest_normal_rnd})",
-            sl=self.target.params.source_vertex.label,
-            tl=self.target_label,
+            sl=self.params.target.params.source_vertex.label,
+            tl=self.params.target_label,
             **trans_params,
-            dest_normal_rnd=self.target_face.rounded_normal_vector.copy(),
+            dest_normal_rnd=self.params.target_face.rounded_normal_vector.copy(),
         )
         logger.debug(
             {
                 **trans_params,
-                "direction_to_origin": self.target.params.direction_to_origin,
+                "direction_to_origin": self.params.target.params.direction_to_origin,
             }
         )
         obj = sutils.transform_to_point(obj, **trans_params)
 
-        if -self.target_face.normal_vector == sutils.UP_VEC:
-            obj = utils.rotate_about_pt(obj, -90, 0, self.destination_point)
+        if -self.params.target_face.normal_vector == sutils.UP_VEC:
+            obj = utils.rotate_about_pt(obj, -90, 0, self.params.destination_point)
         return obj
 
 
@@ -162,16 +186,18 @@ class FixtureLabelDebug(FixtureLabel):
     def create_arrow(
         self, euc_line: Union[EucVector3, EucLine3], color: str, rad: int = 5
     ) -> sp.OpenSCADObject:
-        if not self.destination_point:
+        if not self.params.destination_point:
             raise RuntimeError("No destination point available!")
-        return sp.translate(self.destination_point)(
+        return sp.translate(self.params.destination_point)(
             sutils.draw_segment(euc_line, endless=True, arrow_rad=rad, vec_color=color)
         )
 
     def do_transform(self, obj: sp.OpenSCADObject) -> sp.OpenSCADObject:
         obj = super().do_transform(obj)
         colors = utils.rand_color_generator()
-        axis = self.target.params.direction_to_origin.cross(-self.target_face.normal_vector)
+        axis = self.params.target.params.direction_to_origin.cross(
+            -self.params.target_face.normal_vector
+        )
         obj += self.create_arrow(axis, color=next(colors))
-        obj += self.create_arrow(self.target_face.normal_vector, color=next(colors), rad=3)
+        obj += self.create_arrow(self.params.target_face.normal_vector, color=next(colors), rad=3)
         return obj
