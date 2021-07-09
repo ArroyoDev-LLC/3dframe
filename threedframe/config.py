5d840f82975187825f9677fa7dce038103495512
Braden Mars <bradenmars@bradenmars.me>
2021-07-09 00:11:34 -0500

    feat(config): remove lib imports, copy libs to OpenSCAD path(s), add BOSL2 import

--- threedframe/config.py
+++ threedframe/config.py
@@ -1,35 +1,15 @@
 """3DFrame configuration."""
 
 import shutil
-import contextlib
-from typing import Any, Optional
 from pathlib import Path
 
-import solid
 from pydantic import BaseSettings, validator
-from pydantic.fields import PrivateAttr
 
 from threedframe.constant import Constants
 
 __all__ = ["config"]
 
 ROOT = Path(__file__).parent
-
-
-@contextlib.contextmanager
-def quiet_solid():
-    """Monkey patch solidpython print statements.
-
-    The errors are known and accommodated for,
-    so just ignore them.
-
-    """
-    solid.objects.print = lambda *args: None
-    solid.solidpython.print = lambda *args: None
-    yield
-    # undo monkey patch.
-    solid.objects.print = print
-    solid.solidpython.print = print
 
 
 class _Config(BaseSettings):
@@ -47,61 +27,27 @@ class _Config(BaseSettings):
     # OpenSCAD Poly Segments.
     SEGMENTS: int = 48
     # OpenSCAD Libraries.
-    TMP_DIR: Path = Path("/tmp/") / "threedframe"
     LIB_DIR: Path = ROOT / "lib"
-    MCAD_DIR: Path = LIB_DIR / "MCAD"
-    DOTSCAD_DIR: Path = LIB_DIR / "dotSCAD" / "src"
-
-    _mcad: Optional[Any] = PrivateAttr(None)
-    _dotSCAD: Optional[Any] = PrivateAttr(None)
-
-    @validator("LIB_DIR", "MCAD_DIR", "DOTSCAD_DIR")
-    def validate_libs(cls, v: Path) -> Path:
-        if not v.exists():
-            raise ValueError(f"Missing library: {v}")
-        return v
-
-    @validator("TMP_DIR", pre=True)
-    def validate_tmp_dir(cls, v: Path) -> Path:
-        v.mkdir(exist_ok=True)
-        return v
+    SCAD_LIB_DIR: Path = ROOT.parent / ".local/share/OpenSCAD/libraries"
 
     def create_log_format(self, fmt: str):
         return self.LOG_BASE_FMT + fmt + " - <level>{message}</level>"
 
-    def create_lib_dir(self, lib_dir: Path):
+    def create_lib_dir(self):
         """Create library directory.
 
-        Copies given lib to /tmp/ directory to
-        simplify previewing generated models on host.
+        Copies 3dframe vendor libs to
+        directories searched by OpenSCAD.
 
         """
-        rel_dir = lib_dir.relative_to(self.LIB_DIR)
-        target_dir = self.TMP_DIR / rel_dir
-        if target_dir.exists():
-            return target_dir
-        target_dir.mkdir(parents=True, exist_ok=True)
-        shutil.copytree(lib_dir, target_dir, dirs_exist_ok=True)
-        return target_dir
-
-    def setup_libs(self):
-        mcad = self.create_lib_dir(self.MCAD_DIR)
-        dotscad = self.create_lib_dir(self.DOTSCAD_DIR)
-        with quiet_solid():
-            self._mcad = solid.import_scad(str(mcad))
-            self._dotSCAD = solid.import_scad(str(dotscad))
+        self.SCAD_LIB_DIR.mkdir(parents=True, exist_ok=True)
+        shutil.copytree(self.LIB_DIR, self.SCAD_LIB_DIR, dirs_exist_ok=True)
 
     @property
-    def mcad(self):
-        if not self._mcad:
-            self.setup_libs()
-        return self._mcad
-
-    @property
-    def dotSCAD(self):
-        if not self._dotSCAD:
-            self.setup_libs()
-        return self._dotSCAD
+    def scad_header(self) -> str:
+        self.create_lib_dir()
+        tmpl = "$fn = {c.SEGMENTS};\ninclude <BOSL2/std.scad>\n"
+        return tmpl.format(c=self)
 
     GAP: float = 0.02  # 3dPrinting fudge factor.
 
