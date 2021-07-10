6a7cde8d872ad947770f9d41d33887c02445523e
Braden Mars <bradenmars@bradenmars.me>
2021-07-09 23:14:01 -0500

    feat(scad/interfaces): update CoreMeta to hull vertex cubes via tag

--- threedframe/scad/interfaces.py
+++ threedframe/scad/interfaces.py
@@ -2,7 +2,7 @@ import abc
 from typing import TYPE_CHECKING, Dict, List, Type, Tuple, Iterator, Optional
 
 import attr
-import solid as sp
+import solid.extensions.bosl2.std as bosl2
 from solid.core.object_base import OpenSCADObject
 
 from threedframe import utils
@@ -56,7 +56,7 @@ class CoreMeta(ScadMeta, abc.ABC):
 
     def assemble(self):
         fixture_vertex_cubes = list(self.create_hull_cubes())
-        obj = sp.hull()(*fixture_vertex_cubes)
+        obj: OpenSCADObject = bosl2.hulling("core_pt")(*fixture_vertex_cubes)
         self.scad_object = obj
 
 
