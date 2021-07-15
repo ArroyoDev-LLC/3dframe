59cdf4f3c40517e7c3a05fcdd3d064dc9e4db6f2
Braden Mars <bradenmars@bradenmars.me>
2021-07-15 15:43:17 -0500

    feat(scad/core): implement core name / filename properties

--- threedframe/scad/core.py
+++ threedframe/scad/core.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import attr
 import solid.extensions.bosl2.std as bosl2
 
@@ -6,5 +8,20 @@ from threedframe.scad.interfaces import CoreMeta
 
 @attr.s(auto_attribs=True)
 class Core(CoreMeta):
+    @property
+    def source_label(self) -> Optional[str]:
+        if any(self.fixtures):
+            return self.fixtures[0].params.source_label
+
+    @property
+    def name(self) -> str:
+        source = self.source_label or "?"
+        return f"Core[{source}]"
+
+    @property
+    def file_name(self) -> str:
+        source = self.source_label or "?"
+        return f"core-{source}"
+
     def assemble(self):
         self.scad_object = bosl2.hulling("fix_fillet")(*[f.scad_object for f in self.fixtures])
