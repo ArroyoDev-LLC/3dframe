62ded1c72143682ed64a3193895c982488b5bba0
Braden Mars <bradenmars@bradenmars.me>
2022-08-09 02:41:10 -0500

    feat(scad): create always optimally flushed core via a polyhedron of convex-hull of inner fixture faces

--- threedframe/scad/core.py
+++ threedframe/scad/core.py
@@ -1,13 +1,24 @@
-from typing import Optional
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional
 
 import attr
+import numpy as np
+import solid
 import solid.extensions.bosl2 as bosl2
 
-from threedframe.scad.interfaces import CoreMeta
+from threedframe.utils import distance3d
+from threedframe.scad.interfaces import CoreMeta, scad_timer
+
+if TYPE_CHECKING:
+    import numpy as np
+    import open3d as o3d
 
 
 @attr.s(auto_attribs=True)
 class Core(CoreMeta):
+    verts: list[float] = attr.ib(default=[])
+
     @property
     def source_label(self) -> Optional[str]:
         if any(self.fixtures):
@@ -23,5 +34,30 @@ class Core(CoreMeta):
         source = self.source_label or "?"
         return f"core-{source}"
 
+    @scad_timer
     def assemble(self):
-        self.scad_object = bosl2.conv_hull("fix_fillet")(*[f.scad_object for f in self.fixtures])
+        origin = (0, 0, 0)
+        # TODO: cleanup, optimize, and get rid of assertion.
+        for fixture in self.fixtures:
+            base_mesh: "o3d.geometry.TriangleMesh" = fixture.base_mesh
+            base_mesh.remove_duplicated_vertices()
+            min_bound, max_bound = base_mesh.get_min_bound(), base_mesh.get_max_bound()
+            min_dist = distance3d(origin, min_bound)
+            max_dist = distance3d(origin, max_bound)
+            verts = np.asarray(base_mesh.vertices)
+            inner_verts = []
+            for v in verts.tolist():
+                v_dist = distance3d(origin, v)
+                if (round(v_dist) - round(min_dist)) < (round(max_dist) - round(v_dist)):
+                    inner_verts.append(tuple(v))
+
+            assert len(inner_verts) == 4
+            self.verts.extend(inner_verts)
+        self.scad_object = solid.polyhedron(self.verts, bosl2.hull3d_faces(self.verts))
+
+
+@attr.s(auto_attribs=True)
+class CoreDebugVertices(Core):
+    def assemble(self):
+        super().assemble()
+        self.scad_object += bosl2.move_copies(self.verts)(solid.color("red")(solid.sphere(1)))
