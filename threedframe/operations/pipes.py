e86adfe5a9611de610b86c731e0d185f8994f081
Braden Mars <bradenmars@bradenmars.me>
2022-08-12 03:54:57 -0500

    feat(ops): add Operation protocol, OperationPipeline and OrientMesh/File pipelines

---
+++ threedframe/operations/pipes.py
@@ -0,0 +1,50 @@
+"""Operation Pipes."""
+from __future__ import annotations
+
+from typing import Union, TypeVar, Protocol, runtime_checkable
+from collections import deque
+
+import attrs
+from loguru import logger
+
+import threedframe.operations as op
+
+TargetT = TypeVar("TargetT")
+
+
+@runtime_checkable
+class Operation(Protocol[TargetT]):
+    def operate(self, mesh: TargetT) -> TargetT:
+        ...
+
+
+@attrs.define
+class OperationPipeline:
+    operations: deque[Union[Operation, OperationPipeline]] = attrs.field(factory=deque)
+
+    def add_ops(self, *ops: Union[Operation[TargetT], OperationPipeline]) -> OperationPipeline:
+        self.operations.extend(ops)
+        return self
+
+    def apply(self, target: TargetT) -> TargetT:
+        result = target
+        for op in self.operations:
+            if isinstance(op, OperationPipeline):
+                result = op.apply(result)
+            if isinstance(op, Operation):
+                logger.info("applying operation: {!r}", op)
+                result = op.operate(result)
+            else:
+                raise TypeError("Expected Operation or OperationPipeline object, got: {}", o)
+        return result
+
+
+OrientPipeline = OperationPipeline().add_ops(
+    op.RepairOperation(),
+    op.FlatOperation(),
+    op.OptimalOrientOperation(),
+)
+
+OrientMeshFilePipeline = lambda path: OperationPipeline().add_ops(
+    op.ReadMeshOperation(), op.SerializeMeshOperation(), OrientPipeline, op.WriteMeshOperation(path)
+)
